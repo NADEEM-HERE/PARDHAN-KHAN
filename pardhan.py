7a3d010b35ceb51023176ed58af4e011f8579601
NADEEM-HERE <hellorender420@gmail.com>
2024-09-25 03:10:42 +0530

    Update pardhan.py

--- pardhan.py
+++ pardhan.py
@@ -1,6 +1,6 @@
 #Decode by NADEEM x KAMEENA
 #FACEBOOK https://www.facebook.com/profile.php?id=61565906489735
-#Join more open source https://t.me/nadeemtricker
+#Join more open source https://www.facebook.com/profile.php?id=61565906489735nadeemtricker
 
 
 
@@ -1493,8 +1493,7 @@ logo = """
 888  Y88888   d88P   888 888    888  888   888  Y8P  888      888    888 
 888   Y8888  d8888888888 888  .d88P  888   888   "   888      Y88b  d88P 
 888    Y888 d88P     888 8888888P" 8888888 888       888       "Y8888P88 
-                                                                         
-                                                                                                                                                                        
+                                                                                                                                                                                                                                            
 \033[32m                          
 \033[32m                          
 \033[1;32m                           
